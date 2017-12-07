079d2d6d31a5f6768c728db8c7faeee2b2ddfa17
Nitin Gupta <nitingupta2906@gmail.com>
2017-12-07 13:07:58 +0530

    Create BdayCandle.cpp

---
+++ Mix/BdayCandle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int i,n,c=0;
+	cin>>n;
+	int a[n];
+	for(i=0;i<n;i++)
+	cin>>a[i];
+	sort(a,a+n);
+	for(i=0;i<n;i++){
+		if(a[i]==a[n-1])
+		c++;
+	}
+	cout<<c<<endl;
+}
