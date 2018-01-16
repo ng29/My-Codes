babd74596a6a49a73416f514ca0b5dbd0a85dbf2
Nitin Gupta <nitingupta2906@gmail.com>
2018-01-17 00:00:58 +0530

    Create Nested.java

---
+++ Java/Nested.java
@@ -0,0 +1,39 @@
+class ClassInterface implements My1{
+pubic static void main(String args[]){
+ClassInterface ci=new ClassInterface();
+ci.xyz();
+z.show();
+Inner i = new Inner();
+i.display();
+}
+}
+interface My1{
+deafult void xyz();{
+class Local{
+void localdisplay(){
+System.out.println("local class in interface");
+}
+}
+local l= new local();
+l.localdisplay();
+}
+My z= new My();
+{
+public void show(){
+System.out.println("Annonymous");
+}
+}
+class Inner{
+void dipslay(){
+System.out.println("static nested class in interface");
+}
+}
+}
+class Temp extends My1.Inner{
+public static void main(String args[]){
+new Temp().display();
+}
+}
+inteface My{
+void show();
+}
