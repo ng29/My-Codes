2657355a602fe71b8ba50d71c01bfb94cd751ee8
Nitin Gupta <nitingupta2906@gmail.com>
2018-01-16 23:54:21 +0530

    Create Downcast.java

---
+++ Java/Downcast.java
@@ -0,0 +1,19 @@
+class Base{
+void show(){
+System.out.println("base");
+}
+}
+class Child extends Base{
+void show(){
+System.out.println("child");
+}
+void display(){
+System.out.println("display");
+}
+public static void main(String args[]){
+Base b=new Child();
+b.show();
+Child c=(Child)b;
+c.display();
+}
+}
