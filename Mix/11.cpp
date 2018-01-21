cac42d387f55c6cd9538ec60900f589f5bce0e7c
Nitin Gupta <nitingupta2906@gmail.com>
2018-01-21 14:27:02 +0530

    Create 11.cpp

---
+++ Mix/11.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t,k,unsu,su;
+	cin>>t;
+	cout<<endl;
+	while(t--){
+		int c;
+		cin>>c;
+		while(1){
+			char ch;
+			int i,j;
+			cin>>ch>>i>>j;
+			cout<<endl;
+			int a[c];
+			for(k=0;k<c;k++){
+				a[k]=k;
+			}
+			
+		}
+	}
+}
